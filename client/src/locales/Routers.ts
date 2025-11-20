940fb898ab3202cfce33ebffc226b72469e5adc5
Alikifah89 <font-covert-6r@icloud.com>
2025-11-20 17:39:43 +0300

    Add Router component with lazy-loaded routes

---
+++ client/src/locales/Routers.ts
@@ -0,0 +1,23 @@
+import { Suspense, lazy } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { PageTransition } from './components/PageTransition'
+import { SkeletonHero } from './components/SkeletonLoaders'
+
+const Home = lazy(() => import('./pages/Home'))
+const NotFound = lazy(() => import('./pages/NotFound'))
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <PageTransition>
+        <Suspense fallback={<SkeletonHero />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </PageTransition>
+    </BrowserRouter>
+  )
+}
